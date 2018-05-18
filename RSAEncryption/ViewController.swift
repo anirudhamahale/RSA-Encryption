f61a881052f4689e0445bb65928a293ccb4e9fa3
Anirudha <anirudhamahale@gmail.com>
2018-05-18 15:31:11 +0530

    Commented the code to give better understanding.

--- RSAEncryption/ViewController.swift
+++ RSAEncryption/ViewController.swift
@@ -20,9 +20,10 @@ class ViewController: UIViewController {
         encrypt(string: "This is my message.")
     }
     
-    /*
-     * This function will encrypt the message with RSA encryption.
-     */
+    
+    /// This function will encrypt the message with RSA encryption.
+    ///
+    /// - Parameter string: String that needs to be encrypted.
     func encrypt(string: String) {
         //Encrypt a string with the public key
         let blockSize = SecKeyGetBlockSize(publicKey!)
@@ -37,21 +38,49 @@ class ViewController: UIViewController {
             print("Encryption Error!")
             return
         }
+        
         print("Encrypted")
+        if let base64 = getBase64StringOf(UInt: messageEncrypted) {
+            print("base64 String: ")
+            print(base64)
+        }
     }
     
-    /*
-     * This function will generate the RSA public and private key
-     */
+    
+    /// Generates the random public and private RSA keys and assigns it to the specified property.
+    ///
+    /// publicKey will hold the RSA Public key.
+    ///
+    /// privateKey will hold the RSA Private key.
     func generateSecKeys() {
-        //Generation of RSA private and public keys
+        //Generation of RSA private and public keys.
         let parameters: [String: Any] = [
             kSecAttrKeyType as String : kSecAttrKeyTypeRSA,
             kSecAttrKeySizeInBits as String : 2048
         ]
         
-        // This function will create random keys based on the parameters send to it
+        // This function will create random public, private keys based on the parameters send to it.
         SecKeyGeneratePair(parameters as CFDictionary, &publicKey, &privateKey)
+    }
+    
+    
+    /// This function converts the unsigned integer to Data.
+    ///
+    /// The Data is then converted to encoded to base64 Data.
+    ///
+    /// The base64 Data is encoded to String.
+    ///
+    /// - Parameter UInt: array of 8 bit unsigned integer
+    /// - Returns: The base64 string
+    func getBase64StringOf(UInt: [UInt8]) -> String? {
+        // Convert array of unsigned integer to Data.
+        let data = Data(bytes: UInt)
+        
+        //Encode Data to base64 Data.
+        let base64Data = data.base64EncodedData(options: Data.Base64EncodingOptions.endLineWithLineFeed)
+        
+        // Convert Data to String.
+        return String(data: base64Data, encoding: String.Encoding.utf8)
     }
 }
 
